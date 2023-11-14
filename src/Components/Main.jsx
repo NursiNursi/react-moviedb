a093d9f069aab37b4cb3a5a98f159871e70d8cf9
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-11-14 22:15:04 +0700

    Add: Main component with dummy data for the movie list

---
+++ src/Components/Main.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+export default function Main({ movies }) {
+  return (
+    <main className="main">
+      <Box>
+        <MovieList movies={movies} />
+      </Box>
+    </main>
+  );
+}
